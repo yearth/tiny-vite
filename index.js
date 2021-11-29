3a5038de8e19446ca710df79c363257d01402f7b
yearthmain <yearthmain@gmail.com>
2021-11-29 21:36:55 +0800

    [feat]: 支持模块引入

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const fs = require("fs");
+const path = require("path");
 const app = new Koa();
 
 app.use(ctx => {
@@ -9,6 +10,14 @@ app.use(ctx => {
   if (url === "/") {
     let content = fs.readFileSync("./index.html", "utf-8");
     ctx.type = "text/html";
+    ctx.body = content;
+  }
+
+  // 2. 我们想按需加载，需要支持 import, 首先支持最基础的 .js 文件
+  if (url.endsWith(".js")) {
+    let p = path.resolve(__dirname, url.slice(1));
+    let content = fs.readFileSync(p, "utf-8");
+    ctx.type = "application/javascript";
     ctx.body = content;
   }
 });
